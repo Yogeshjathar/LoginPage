5a72f548ee1d069886845b7f0523b5fcbd0caff1
Yogesh_Jathar <jatharyogeshv@gmail.com>
2024-02-02 18:15:07 +0530

    Login page completed

--- js/main.js
+++ js/main.js
@@ -5,20 +5,20 @@ const showHiddenPass = (loginPass, loginEye) => {
           iconEye.addEventListener('click',() =>{
             if(input.type === 'password'){
                 // Switch to text
-                input.type === 'text'
+                input.type = 'text'
 
                 // Icon change
                 iconEye.classList.add('ri-eye-line')
                 iconEye.classList.remove('ri-eye-off-line')
             }else{
                 // Switch to password
-                input.type === 'password'
+                input.type = 'password'
 
                 // Icon change
-                iconEye.classList.add('ri-eye-off-line')
                 iconEye.classList.remove('ri-eye-line')
+                iconEye.classList.add('ri-eye-off-line')
             }
           })
 }
 
-showHiddenPass('loginPass','login_Eye')
+showHiddenPass('loginPass','loginEye')
